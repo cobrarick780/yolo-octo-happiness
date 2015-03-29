8bf24b57057e1c03db7dda4a5c8c59cc659e8a76
cobrarick780 <tooga_googa@yahoo.com>
2015-03-29 15:47:38 -0700

    Create Particle.java

---
+++ Particle.java
@@ -0,0 +1,12 @@
+package engine;
+
+public class Particle {
+	public int R,G,B;
+	public static final double SIZE = 20;
+	
+	public Particle(int R, int G, int B){
+		this.R = R;
+		this.G = G;
+		this.B = B;
+	}
+}
